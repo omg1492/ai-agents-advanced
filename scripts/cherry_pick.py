336406e8610864f52b67b73d53582e80c7869aba
Tomas Kubica <tkubica12@gmail.com>
2025-10-29 13:45:03 +0100

    cherry pick script

--- scripts/cherry_pick.py
+++ scripts/cherry_pick.py
@@ -145,8 +145,7 @@ def prompt_confirm(commit: CommitInfo, main_branch: str, assume_yes: bool = Fals
 
 LESSON_BRANCH_PATTERNS = [
 	re.compile(r"^L\d{2}-teacher$"),
-	re.compile(r"^L\d{2}-student-starter$"),
-	re.compile(r"^L\d{2}-student-end$"),
+	re.compile(r"^L\d{2}-student-$"),
 ]
 
 
